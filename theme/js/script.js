ea3ddde2a38ce3b3ea218f313db5c16b14104c3a
Ines Garcia Ortiz <ines.garciaortiz99@gmail.com>
2023-01-09 12:12:59 +0100

    nuevo intento

--- theme/js/script.js
+++ theme/js/script.js
@@ -5,46 +5,30 @@
  * GITHUB: https://github.com/themefisher/
  */
 
-(function ($) {
-  'use strict';
-
-  // PRELOADER
-  $(window).on('load', function () {
-    $('#page-loader').fadeOut('slow', function () {
-      $(this).remove();
-    });
-  });
+$(document).ready(function () {
+	'use strict';
 
 	// navbarDropdown
 	if ($(window).width() < 992) {
-		$('.has-dropdown .dropdown-toggle').on('click', function () {
+		$('.navigation .dropdown-toggle').on('click', function () {
 			$(this).siblings('.dropdown-menu').animate({
 				height: 'toggle'
 			}, 300);
 		});
 	}
 
-
-  // SCROLL TO TOP
-  $(window).on('scroll', function () {
-    if ($(window).scrollTop() > 70) {
-      $('.scroll-to-top').addClass('reveal');
-    } else {
-      $('.scroll-to-top').removeClass('reveal');
-    }
-  });
-
-
-  // Fixed header
-  $(window).on('scroll', function () {
-    if ($(window).scrollTop() > 70) {
-      $('.site-navigation,.trans-navigation').addClass('header-white');
-    } else {
-      $('.site-navigation,.trans-navigation').removeClass('header-white');
-    }
-  });
-  
-
+	$(window).on('scroll', function () {
+		//.Scroll to top show/hide
+		if ($('#scroll-to-top').length) {
+			var scrollToTop = $('#scroll-to-top'),
+				scroll = $(window).scrollTop();
+			if (scroll >= 200) {
+				scrollToTop.fadeIn(200);
+			} else {
+				scrollToTop.fadeOut(100);
+			}
+		}
+	});
 	// scroll-to-top
 	if ($('#scroll-to-top').length) {
 		$('#scroll-to-top').on('click', function () {
@@ -55,10 +39,104 @@
 		});
 	}
 
+	// Shuffle js filter and masonry
+	var containerEl = document.querySelector('.shuffle-wrapper');
+	if (containerEl) {
+		var Shuffle = window.Shuffle;
+		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+			itemSelector: '.shuffle-item',
+			buffer: 1
+		});
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').on('click', function (event) {
-    $('.navbar-collapse').collapse('hide');
-  });
+		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+			var input = evt.currentTarget;
+			if (input.checked) {
+				myShuffle.filter(input.value);
+			}
+		});
+	}
 
-})(jQuery);
+	$('.portfolio-single-slider').slick({
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000
+	});
+
+	$('.clients-logo').slick({
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000
+	});
+
+	$('.testimonial-slider').slick({
+		slidesToShow: 1,
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000
+	});
+
+
+	// CountDown JS
+	var countDownEl = $('.count-down');
+	if (countDownEl) {
+		$('.count-down').syotimer({
+			year: 2021,
+			month: 5,
+			day: 9,
+			hour: 20,
+			minute: 30
+		});
+	}
+
+	// Magnific Popup Image
+	$('.portfolio-popup').magnificPopup({
+		type: 'image',
+		removalDelay: 160, //delay removal by X to allow out-animation
+		callbacks: {
+			beforeOpen: function () {
+				// just a hack that adds mfp-anim class to markup
+				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+				this.st.mainClass = this.st.el.attr('data-effect');
+			}
+		},
+		closeOnContentClick: true,
+		midClick: true,
+		fixedContentPos: true,
+		fixedBgPos: true
+	});
+
+	//  Count Up
+	function counter() {
+		var oTop;
+		if ($('.count').length !== 0) {
+			oTop = $('.count').offset().top - window.innerHeight;
+		}
+		if ($(window).scrollTop() > oTop) {
+			$('.count').each(function () {
+				var $this = $(this),
+					countTo = $this.attr('data-count');
+				$({
+					countNum: $this.text()
+				}).animate({
+					countNum: countTo
+				}, {
+					duration: 1000,
+					easing: 'swing',
+					step: function () {
+						$this.text(Math.floor(this.countNum));
+					},
+					complete: function () {
+						$this.text(this.countNum);
+					}
+				});
+			});
+		}
+	}
+	$(window).on('scroll', function () {
+		counter();
+	});
+
+});
